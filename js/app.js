59e1294e53eaf354381fa24dd7643c31e33b3822
Jonathan <jonathanallen105@gmail.com>
2025-08-08 15:44:05 -0400

    completed level up excersies 10 and 11

--- js/app.js
+++ js/app.js
@@ -197,3 +197,70 @@ const basicCalculator = (num1, num2, operator) => {
 }
 
 console.log('Exercise 9 Result:', basicCalculator(10, 5, "divide"));
+
+/*
+Exercise 10: calculateGrade()
+
+Define a function called calculateGrade. 
+It should take a numerical score and return the corresponding letter 
+grade (A, B, C, D, F). 
+
+For example, 90 and above yields an 'A', 80-89 is a 'B', 
+and 70-79 is a 'C', 60-69 is a 'D' and anything lower than a 60 is an 'F'.
+
+Example: calculateGrade(100) should return A.
+
+Complete the exercise in the space below:
+*/
+
+const calculateGrade = grade => {
+    if (grade > 89) {
+        return 'A'
+    } else if (grade < 90 && grade > 79) {
+        return 'B'
+    } else if (grade < 80 && grade > 69) {
+        return 'C'
+    } else if (grade < 70 && grade > 59) {
+        return 'D'
+    } else {
+        return 'F'
+    }
+}
+
+console.log('Exercise 10 Result:', calculateGrade(99));
+
+/*
+Exercise 11: createUsername()
+
+Define a function called createUsername. 
+It should take a first name and a last name and return a username. 
+
+The username should be a combination of the following:
+- The first three letters of the first name.
+- The first three letters of the last name.
+- The total character count of the first and last name combined.
+
+Example: createUsername('Samantha', 'Green') should return 'SamGre13'.
+
+Complete the exercise in the space below:
+*/
+
+const createUsername = (firstName, lastName) => {
+    fullName = firstName + lastName
+    return `${firstName.substring(0,3)}${lastName.substring(0,3)}${fullName.length}`
+}
+
+console.log('Exercise 11 Result:', createUsername("Jonathan", "Allen"));
+
+/*
+Exercise 12: numArgs()
+
+Challenge yourself with numArgs. 
+This function should return the count of arguments passed to it when called.
+
+Complete the exercise in the space below:
+*/
+
+const numArgs = 
+
+console.log('Exercise 12 Result:', numArgs(1, 2, 3, 4));
