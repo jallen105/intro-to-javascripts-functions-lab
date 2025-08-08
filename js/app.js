7d932b61d18525dcd286a0071a1f4aa940f3a427
Jonathan <jonathanallen105@gmail.com>
2025-08-08 14:37:07 -0400

    completed exercise 6

--- js/app.js
+++ js/app.js
@@ -103,7 +103,17 @@ Example: maxOfThree(17, 4, 9) should return 17.
 Complete the exercise in the space below:
 */
 
-
+const maxOfThree = (num1, num2, num3) => {
+    if (num1 > num2 && num1 < num3){
+        return num1
+    } else if (num2 > num1 && num2 > num3) {
+        return num2
+    }else if (num3 > num1 && num3 > num2) {
+        return num3
+    }else {
+        return num1
+    }
+}
 
 console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
 
